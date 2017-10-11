c712d4fb99af683f98fa59a82e8dbb28eac48f11
SvetlanaSm <32055211+SvetlanaSm@users.noreply.github.com>
2017-10-11 15:00:30 +0500

    Задание на нахождение расстояния между точками

---
+++ src/Distance.java
@@ -0,0 +1,8 @@
+public class Distance {
+
+   public static double distance(Point p1, Point p2){
+   Point A = new Point;
+   Point B = new Point;
+   p = Math.sqrt((p.x - p.x) * (p.x1 - p.x2) + (p.y1 - p.y1) * (p.y1 - p.y1));
+   }
+}
